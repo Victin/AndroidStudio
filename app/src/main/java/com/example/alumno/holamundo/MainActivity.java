4c4579db4a127528abf76dfac2fbc21148c5740b
Viti <victorgomezes@hotmail.com>
2017-10-16 12:34:32 +0200

    HolaMundo v1

--- app/src/main/java/com/example/alumno/holamundo/MainActivity.java
+++ app/src/main/java/com/example/alumno/holamundo/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear_layout);
+        setContentView(R.layout.activity_main_ejercicio_1);
     }
 }
